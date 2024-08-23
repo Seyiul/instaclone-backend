7e6693c6e17d6cde6cef1008e947dc1851faa953
Lee Seul Yi <63714074+Seyiul@users.noreply.github.com>
2024-08-23 10:32:51 +0900

    see feed

---
+++ src/photos/seeFeed/seeFeed.resolvers.ts
@@ -0,0 +1,29 @@
+import { protectedResolver } from "../../users/user.utils";
+
+export default {
+  Query: {
+    seeFeed: protectedResolver((_, __, { client, loggedInUser }) =>
+      client.photo.findMany({
+        where: {
+          OR: [
+            {
+              user: {
+                followers: {
+                  some: {
+                    id: loggedInUser.id,
+                  },
+                },
+              },
+            },
+            {
+              userId: loggedInUser.id,
+            },
+          ],
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      })
+    ),
+  },
+};
