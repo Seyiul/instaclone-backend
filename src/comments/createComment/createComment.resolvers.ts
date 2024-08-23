f2d5dbce1f8503216a161b581419491cc041cdfc
Lee Seul Yi <63714074+Seyiul@users.noreply.github.com>
2024-08-23 13:29:16 +0900

    comment on photo

---
+++ src/comments/createComment/createComment.resolvers.ts
@@ -0,0 +1,45 @@
+import { protectedResolver } from "../../users/user.utils";
+
+export default {
+  Mutation: {
+    createComment: protectedResolver(
+      async (_, { photoId, payload }, { client, loggedInUser }) => {
+        const ok = await client.photo.findUnique({
+          where: {
+            id: photoId,
+          },
+          select: {
+            id: true,
+          },
+        });
+
+        if (!ok) {
+          return {
+            ok: false,
+            error: "Photo not Found.",
+          };
+        }
+
+        await client.comment.create({
+          data: {
+            payload,
+            photo: {
+              connect: {
+                id: photoId,
+              },
+            },
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+          },
+        });
+
+        return {
+          ok: true,
+        };
+      }
+    ),
+  },
+};
