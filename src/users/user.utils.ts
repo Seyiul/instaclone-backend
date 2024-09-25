3519e7e6b13ba616629fb59b1774121a69b8194d
Lee Seul Yi <63714074+Seyiul@users.noreply.github.com>
2024-09-25 17:48:10 +0900

    me resolvers

--- src/users/user.utils.ts
+++ src/users/user.utils.ts
@@ -5,6 +5,8 @@ import { GraphQLResolveInfo } from "graphql";
 export const getUser = async (token) => {
   try {
     if (!token) {
+      console.log("KOKOKOK");
+
       return null;
     }
     const verifiedToken: any = await jwt.verify(token, process.env.SECRET_KEY);
